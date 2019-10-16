bcbb93b81c11ad16964bc88f591b092c380ac859
notmaiku <notmaiku@protonmail.com>
2019-10-16 16:36:51 -0400

    Initial commit

---
+++ ClientApp/components/Bugs.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+interface MsgState {
+    msg: string;
+}
+
+export class Bugs extends React.Component<RouteComponentProps<{}>, MsgState> {
+    constructor() {
+        super();
+        this.state = { msg: 'Hi stinky' };
+    }
+
+    public render() {
+        return <div>
+            <h1>Counter</h1>
+
+            <p>This is a simple example of a React component.</p>
+
+            <p>I came here to say: <strong>{ this.state.msg }</strong></p>
+
+        </div>;
+    }
+}
